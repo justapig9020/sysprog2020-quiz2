472aca4c1e28d8c9beb090b74ad1ec1ab6af3617
justapig9020 <justapig9020@gmail.com>
2020-09-17 13:08:42 +0800

    Update memory alignment feature for string check

--- question1/topic2/main.c
+++ question1/topic2/main.c
@@ -2,6 +2,7 @@
 #include <stdint.h>
 #include <stdio.h>
 #define ARR_SIZE(a) (sizeof(a) / sizeof(a[0]))
+#define ALIGN(ptr) (((ptr) + sizeof(uint64_t) - 1) & ~(sizeof(uint64_t) - 1))
 
 const uint64_t MASK = 0x0101010101010101;
 char BUF[100];
@@ -22,14 +23,26 @@ bool isalph_ward(uint64_t w) {
 
 bool isalph_str(char str[], size_t size) {
     bool alph = true;
+    char *aligned = (char *)ALIGN((uint64_t)(str));
 
     printf("size: %zu\n", size);
+    printf("string: %p\n", str);
+    printf("Aligned: %p\n", aligned);
+
+    while (alph && str < aligned) {
+        alph = isalph_char(*str);
+        str++;
+        size--;
+    }
+
+    printf("Pre-matched: %p\n", str);
     while (alph && size >= 8) {
-        alph = isalph_ward(*(uint64_t*)str);
+        alph = isalph_ward(*(uint64_t *)str);
         str += sizeof(uint64_t);
         size -= 8;
     }
 
+    printf("Matched ward: %p\n", str);
     while (alph && size) {
         alph = isalph_char(*str);
         str++;
@@ -42,14 +55,13 @@ bool isalph_str(char str[], size_t size) {
 bool isbreak(char c) { return c == '\0' || c == '\n'; }
 
 int main(void) {
-    size_t i = 0;
+    size_t len = 0;
     do {
-        BUF[i] = getchar();
-    } while (i < ARR_SIZE(BUF) - 1 && !isbreak(BUF[i++]));
-    BUF[i] = '\0';
+        BUF[len] = getchar();
+    } while (len < ARR_SIZE(BUF) - 1 && !isbreak(BUF[len++]));
+    BUF[len] = '\0';
 
-    bool alph = isalph_str(BUF, i - 1);
-
+    bool alph = isalph_str(BUF, len - 1);
     if (alph)
         puts("Shark!!!");
     else
